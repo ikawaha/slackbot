8e92edde743c309834782eb3cbbc45087b5cf899
ikawaha <ikawaha@users.noreply.github.com>
2015-09-30 18:24:16 +0900

    add new commands

---
+++ cmd/echo/echobot.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/ikawaha/slackbot"
+)
+
+// echo sample
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "usage: bot slack-bot-token\n")
+		os.Exit(1)
+	}
+
+	// start a websocket-based Real Time API session
+	bot, err := slackbot.New(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	bot.Close()
+	fmt.Println("^C exits")
+
+	for {
+		msg, err := bot.GetMessage()
+		if err != nil {
+			log.Printf("receive error, %v", err)
+		}
+		log.Printf("bot_id: %v, msguser_id: %v, msg:%+v\n", bot.ID, msg.UserID, msg)
+		if bot.ID == msg.MentionID() && msg.Type == "message" && msg.SubType == "" {
+			go func(m slackbot.Message) {
+				m.Text = m.TextBody()
+				bot.PostMessage(m)
+			}(msg)
+		}
+	}
+}
---
+++ cmd/haiku/haikubot.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/mattn/go-haiku" // awesome!
+
+	"github.com/ikawaha/slackbot"
+)
+
+const (
+	botResponseSleepTime = 3 * time.Second
+)
+
+// haiku sample
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "usage: bot slack-bot-token\n")
+		os.Exit(1)
+	}
+
+	// start a websocket-based Real Time API session
+	bot, err := slackbot.New(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer bot.Close()
+	fmt.Println("^C exits")
+
+	r575 := []int{5, 7, 5}
+	for {
+		msg, err := bot.GetMessage()
+		if err != nil {
+			log.Printf("receive error, %v", err)
+		}
+		log.Printf("bot_id: %v, msguser_id: %v, msg:%+v\n", bot.ID, msg.UserID, msg)
+		if msg.Type != "message" {
+			continue
+		}
+		go func(m slackbot.Message) {
+			t := m.TextBody()
+			hs := haiku.Find(t, r575)
+			if len(hs) < 1 {
+				return
+			}
+			var tmp []string
+			for _, h := range haikus {
+				tmp = append(tmp, fmt.Sprintf("```%v```", h))
+			}
+			m.Text = strings.Join(tmp, "\n")
+			m.Text += " 575だ"
+			time.Sleep(botResponseSleepTime)
+			bot.PostMessage(m)
+		}(msg, hs)
+	}
+}
